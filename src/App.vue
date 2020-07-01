fc36029a47144752a3ef30eed72401cf9f0b493f
Pa7r1Q ForeveR <patro11@hotmail.com>
2020-07-01 10:41:20 +0200

    Added store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo"/>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -25,4 +25,20 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+* {
+  box-sizing: border-box;
+}
+
+.container{
+max-width: 600px;
+margin : 0 auto;
+}
+
+.logo{
+display:block;
+margin: 20px auto;
+height: 75px;
+}
+
 </style>
---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+import Vue from "vue"
+import Vuex from "vuex"
+//import axios from "axios"
+
+//Load Vuex
+Vue.use(Vuex)
+
+//Initialize store
+export const store = new Vuex.Store({
+    state:{
+
+    },
+    getters:{
+
+
+    },
+    actions:{
+
+    },
+    mutations:{
+        
+    }
+})
