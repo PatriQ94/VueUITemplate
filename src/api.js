558a66176767efc5d1231547eb38c8ec00904b01
Pa7r1Q ForeveR <patro11@hotmail.com>
2020-07-11 09:32:38 +0200

    Splitted router to own module

--- src/api.js
+++ src/api.js
@@ -1,19 +1,45 @@
 import axios from "axios";
-// import { store } from "./store";
+import { store } from "./store";
+import router from "./router";
 
 let api = axios.create({
   baseURL: "https://localhost:5001",
 });
 
+// Add a request interceptor
+api.interceptors.request.use(function(config) {
+  if (store.getters.loggedIn) {
+    config.headers.Authorization = "Bearer " + store.getters.getAccessToken;
+  } else {
+    config.headers.Authorization = null;
+  }
+  return config;
+});
+
 // Add a response interceptor
 api.interceptors.response.use(
   function(response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    console.log("This is some axios interceptor 1");
+    //Successful response
     return response;
   },
   function(error) {
+    // Return any error which is not due to authentication
+    if (error.response.status !== 401) {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+
+    // Logout user if token refresh didn't work
+    if (error.config.url == "/api/Auth/RefreshToken") {
+      store.dispatch("logout").then(() => {
+        router.push({ name: "login" });
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      });
+    }
+
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     console.log("This is some axios interceptor 2 for error");
--- src/main.js
+++ src/main.js
@@ -1,44 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueRouter from "vue-router";
 import { store } from "./store";
-import routes from "./routes";
 import vuetify from "./plugins/vuetify";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
-
-Vue.config.productionTip = false;
-
-//Set up VueRouter
-Vue.use(VueRouter);
-const router = new VueRouter({
-  routes,
-  mode: "history",
-});
-router.beforeEach((to, from, next) => {
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-    if (!store.getters.loggedIn) {
-      //Redirect to login route
-      next({
-        name: "login",
-      });
-    } else {
-      next();
-    }
-  } else if (to.matched.some((record) => record.meta.requiresVisitor)) {
-    if (store.getters.loggedIn) {
-      //Redirect to garage route
-      next({
-        name: "garage",
-      });
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
-});
+import router from "./router";
 
 new Vue({
   store,
